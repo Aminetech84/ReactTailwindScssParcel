5f1994225b9fe2a5541f06155a6a0d2a0a0b412a
Amine Ouldaouali <aminetech84@hotmail.com>
2020-06-15 22:41:42 +0100

    V 0.0.2

---
+++ src/components/Div1.js
@@ -0,0 +1,45 @@
+/*Components for Layouts*/
+
+import React, { useEffect, useRef, Component } from 'react';
+import Style from '../../public/style/main.css';
+
+import { TweenMax, Power3, Power2, gsap } from 'gsap';
+
+function Div1() {
+    //console.log(props.class);
+    
+    /*let navBar = useRef(null);
+    let h1 = useRef(null);
+    let hr = useRef(null);*/
+
+
+    useEffect(() => {
+      /*  console.log(navBar);
+        let tl = gsap.timeline({
+            delay: 0,
+        });
+        tl.from([navBar, h1], 2, { opacity: 0,  ease: Power2.easeInOut })
+          .from(hr, 2, { width: 0,  ease: Power2.easeInOut })
+          //.from(header, 2, { height: null, ease: Power2.easeInOut })
+*/
+    }, [])
+
+
+
+    return (
+       <div></div>
+    )
+
+
+}
+
+/*
+
+
+const col1 = {
+    backgroundColor: '#ccc'
+}
+ * 
+ */
+
+export default Div1
---
+++ src/layouts/Header.js
@@ -0,0 +1,39 @@
+/**
+ * Building blocks of this level would usually be from components
+ * and for pages
+ */
+
+
+
+import React, { useEffect, useRef } from 'react'
+import Style from '../../public/style/main.css'
+import { TweenMax, Power3, Power2, gsap } from 'gsap';
+import pic from '../../public/images/pic1.jpg';
+import vid from '../../public/videos/Haerfest_Website_960.mp4'
+
+
+function Header() {
+   
+
+    return (
+
+        <div>
+
+        </div>
+
+
+    )
+}
+
+/**
+ * 
+ *  
+ * style={headerColor} 
+ * const headerColor = { backgroundColor: "#6d7a71" }
+ * const headerColor = () => width < 500? 'bg-blue-900' : 'bg-red-300'
+ */
+
+
+
+
+export default Header
